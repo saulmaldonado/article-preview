3735da11b8105dd59ef6c840d20f5ce74ebee3c5
Saul Maldonado <samaldonado4@gmail.com>
2020-08-09 12:38:18 -0500

    Add and style image and article data

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import styles from './styles.module.less'
+// import { ReactComponent as Facebook } from './images/icon-facebook.svg'
+import { ReactComponent as Share } from './images/icon-share.svg'
 
 interface Props {
   title: string
@@ -11,11 +13,33 @@ interface Props {
   socialLinks: string[]
 }
 
-export const ArticlePreview = ({}: Props) => {
+export const ArticlePreview = ({
+  title,
+  image,
+  subtitle,
+  profilePicture,
+  author,
+  datePosted,
+}: Props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.image}></div>
-      <div className={styles.info}></div>
+      <div className={styles.image}>
+        <img src={image} alt={`${title}-image`} />
+      </div>
+      <div className={styles.info}>
+        <header className={styles.title}>{title}</header>
+        <p className={styles.subtitle}>{subtitle}</p>
+        <div className={styles.author}>
+          <img src={profilePicture} alt={`${author}-profile-picture`} />
+          <div className={styles.dataContainer}>
+            <p className={styles.name}>{author}</p>
+            <p className={styles.date}>{datePosted.toDateString()}</p>
+          </div>
+          <div className={styles.share}>
+            <Share />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
