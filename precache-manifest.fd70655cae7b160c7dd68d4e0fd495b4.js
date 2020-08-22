a3a8618a38973f2ee5faa107f8dc15c285aac022
Saul Maldonado <samaldonado4@gmail.com>
2020-08-22 17:31:26 -0500

    Updates

---
+++ precache-manifest.fd70655cae7b160c7dd68d4e0fd495b4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4d87f1f1193cb1aece454f22846beb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a68d0e27a64b0c99cc6e",
+    "url": "./static/css/main.226d6612.chunk.css"
+  },
+  {
+    "revision": "8d9acbe90abb4da31e7c",
+    "url": "./static/js/2.38583ef5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.38583ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a68d0e27a64b0c99cc6e",
+    "url": "./static/js/main.d0a589b3.chunk.js"
+  },
+  {
+    "revision": "cf8b28941960993cdb01",
+    "url": "./static/js/runtime-main.e20573e8.js"
+  },
+  {
+    "revision": "bcb67475ab1fda7673c4c9fd3ca9d09f",
+    "url": "./static/media/drawers.bcb67475.jpg"
+  },
+  {
+    "revision": "f26a669d4e3d192dc45a610d9553be9f",
+    "url": "./static/media/icon-facebook-messenger~BCjvfbxi.f26a669d.svg"
+  },
+  {
+    "revision": "87fd60e59c9244c5c89559879e737a90",
+    "url": "./static/media/icon-facebook~SvkjyEeY.87fd60e5.svg"
+  },
+  {
+    "revision": "17a218044953493b91243e8cce376ef6",
+    "url": "./static/media/icon-linkedin~VopiwcKx.17a21804.svg"
+  },
+  {
+    "revision": "672fe61273317711c0566e94583aab0c",
+    "url": "./static/media/icon-pinterest~biZsyYkg.672fe612.svg"
+  },
+  {
+    "revision": "75abb0b19c5367e5a23e8451310226e6",
+    "url": "./static/media/icon-reddit~ApPpXucD.75abb0b1.svg"
+  },
+  {
+    "revision": "fd590c632c10555bb74c7e739fc19724",
+    "url": "./static/media/icon-share~GurnUaYD.fd590c63.svg"
+  },
+  {
+    "revision": "4013bcc95663ffcd51a1f7343d983bb6",
+    "url": "./static/media/icon-twitter~UkJKCAvz.4013bcc9.svg"
+  }
+]);
